4db3bbdde51b26b7d1ec03ad927ec08c1d2cf976
sandhyaveludandi <sdvelu18@gmail.com>
2022-06-23 23:15:45 -0400

    fix: added comma after new role

---
+++ src/components/Footer.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer>
+    <Particles 
+    id="tsparticles"
+    :options="{
+        particles: {
+        number: {
+            density: {
+            enable: false,
+            value_area: 1500,
+            },
+            value: 50,
+        },
+        shape: {
+            type: 'star',
+            stroke: {
+            width: 0.8,
+            color: '#45f0df'
+            }
+        },
+        line_linked: {
+            enable: false, 
+            distance: 90, 
+            opacity: 1, 
+            width: 1.5
+        },
+        move: {
+            enable: true,
+            direction: 'right',
+            random: true,
+            straight: true,
+            speed: 0.25,
+            outMode: 'out'      
+        },
+        size: {
+            anim: {
+            speed: 100, 
+            size_min: 0.03
+            },
+            random: true,
+            value: 1.25,
+        },
+        opacity: {
+            anim: {
+                enable: true,
+                speed: 1.25,
+                opacity_min: 0.3
+            },
+            value: 1,
+            random: true
+        }
+        },
+        background: {
+        color: '#111d4A'
+        },
+        fullScreen: {
+            enable: false
+        },
+        fpsLimit: 60,
+        interactivity: {
+        detectsOn: 'canvas'
+        },
+        detectRetina: true,
+    }"
+    />
+        <div class="flex-container">
+            <a href="tel:3392241910"><v-icon name="fa-phone-square-alt" scale="2"/></a>
+            <a href="mailto:sandhya-veludandi@pm.me"><v-icon name="hi-solid-mail" scale="2"/></a>
+            <a href="https://www.linkedin.com/in/sandhya-veludandi"><v-icon name="io-logo-linkedin" scale="2"/></a>
+            <a href="https://github.com/sandhyaveludandi"><v-icon name="fa-github-square" scale="2"/></a>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+canvas {
+    display: block; 
+}
+
+#tsparticles {
+position: absolute; 
+height: 7%;
+width: 100%;
+z-index: -1; 
+}
+
+.flex-container {
+    margin-left: 9em; 
+    margin-right: 9em;  
+    padding: 0.45em; 
+    display: flex;
+    justify-content: space-evenly;  
+}
+
+a {
+    text-decoration: none; 
+}
+
+a:visited {
+    color: #d6dce0 ;
+}
+
+.ov-icon {
+    color: white; 
+}
+
+.ov-icon:hover {
+  cursor: pointer;
+  color: #d6dce0;
+}
+
+</style>
---
+++ src/components/Sections.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main class="main">
+        <section class="slide" id="tab-resume">
+            <iframe src="https://drive.google.com/file/d/1mzODkzTdz_HydMdlZSKzA5iIzLG0nL9U/preview" width="640" height="480" allow="autoplay"></iframe>
+        </section>
+    </main>
+</template>
+
+<style>
+main {
+background-color: #C2CAE8;
+}
+.tabs,
+.slide {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+padding: 0 2em;
+}
+  
+.slide h1 {
+font-size: 2rem;
+margin: 0;
+letter-spacing: 1rem;
+}
+
+#tab-resume {
+height: 175vh;
+}
+
+iframe {
+margin: 0; 
+padding: 0; 
+border: 0; 
+width: 800px;  
+height: 1000px; 
+overflow: hidden; 
+}
+
+</style>
